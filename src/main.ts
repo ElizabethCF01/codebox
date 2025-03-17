2b740c3def34eb097d76d109714f3f0046b23b0e
ElizabethCF01 <elizabethcalzadillafernandez@gmail.com>
2025-03-17 11:09:17 +0100

    add: tailwind css and vue prism js for highlight code

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { PrismEditor } from 'vue-prism-editor'
+import 'vue-prism-editor/dist/prismeditor.min.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.component('PrismEditor', PrismEditor)
 app.mount('#app')
